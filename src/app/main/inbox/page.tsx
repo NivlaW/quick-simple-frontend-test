65095c5f7b205e1c7ce707baa1e34b84140f9472
Alvin Noval Mazzaky <ojodumeh337@gmail.com>
2023-12-10 20:23:01 +0700

    update ui inbox

--- src/app/main/inbox/page.tsx
+++ src/app/main/inbox/page.tsx
@@ -28,29 +28,54 @@ export default function InboxPage() {
             return (
               <>
                 <Link className="flex p-5" href="/main/inbox/detail">
-                  <div className="flex flex-cols-3 gap-5 items-center">
-                    {x == 2 ? (
-                      <div className="flex">
-                        <Avatar>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="12"
-                            height="12"
-                            viewBox="0 0 12 12"
-                            fill="none"
+                  <div className="flex w-full items-start justify-between gap-5">
+                    <div className="flex gap-5 items-center">
+                      {x == 2 ? (
+                        <div className="flex">
+                          <Avatar>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="12"
+                              height="12"
+                              viewBox="0 0 12 12"
+                              fill="none"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                clip-rule="evenodd"
+                                d="M6 0C4.3425 0 3 1.3425 3 3C3 4.6575 4.3425 6 6 6C7.6575 6 9 4.6575 9 3C9 1.3425 7.6575 0 6 0ZM7.5 3C7.5 2.175 6.825 1.5 6 1.5C5.175 1.5 4.5 2.175 4.5 3C4.5 3.825 5.175 4.5 6 4.5C6.825 4.5 7.5 3.825 7.5 3ZM10.5 10.5C10.35 9.9675 8.025 9 6 9C3.9825 9 1.6725 9.96 1.5 10.5H10.5ZM0 10.5C0 8.505 3.9975 7.5 6 7.5C8.0025 7.5 12 8.505 12 10.5V12H0V10.5Z"
+                                fill="black"
+                                fill-opacity="0.54"
+                              />
+                            </svg>
+                          </Avatar>
+                          <Avatar
+                            sx={{
+                              marginInlineStart: "-1rem",
+                              bgcolor: "#2F80ED",
+                            }}
                           >
-                            <path
-                              fill-rule="evenodd"
-                              clip-rule="evenodd"
-                              d="M6 0C4.3425 0 3 1.3425 3 3C3 4.6575 4.3425 6 6 6C7.6575 6 9 4.6575 9 3C9 1.3425 7.6575 0 6 0ZM7.5 3C7.5 2.175 6.825 1.5 6 1.5C5.175 1.5 4.5 2.175 4.5 3C4.5 3.825 5.175 4.5 6 4.5C6.825 4.5 7.5 3.825 7.5 3ZM10.5 10.5C10.35 9.9675 8.025 9 6 9C3.9825 9 1.6725 9.96 1.5 10.5H10.5ZM0 10.5C0 8.505 3.9975 7.5 6 7.5C8.0025 7.5 12 8.505 12 10.5V12H0V10.5Z"
-                              fill="black"
-                              fill-opacity="0.54"
-                            />
-                          </svg>
-                        </Avatar>
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              width="12"
+                              height="12"
+                              viewBox="0 0 12 12"
+                              fill="none"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                clip-rule="evenodd"
+                                d="M6 0C4.3425 0 3 1.3425 3 3C3 4.6575 4.3425 6 6 6C7.6575 6 9 4.6575 9 3C9 1.3425 7.6575 0 6 0ZM7.5 3C7.5 2.175 6.825 1.5 6 1.5C5.175 1.5 4.5 2.175 4.5 3C4.5 3.825 5.175 4.5 6 4.5C6.825 4.5 7.5 3.825 7.5 3ZM10.5 10.5C10.35 9.9675 8.025 9 6 9C3.9825 9 1.6725 9.96 1.5 10.5H10.5ZM0 10.5C0 8.505 3.9975 7.5 6 7.5C8.0025 7.5 12 8.505 12 10.5V12H0V10.5Z"
+                                fill="white"
+                                fill-opacity="0.54"
+                              />
+                            </svg>
+                          </Avatar>
+                        </div>
+                      ) : (
                         <Avatar
                           sx={{
-                            marginInlineStart: "-1rem",
+                            marginX: "0.7rem",
                             bgcolor: "#2F80ED",
                           }}
                         >
@@ -70,40 +95,22 @@ export default function InboxPage() {
                             />
                           </svg>
                         </Avatar>
+                      )}
+                      <div className="flex flex-col">
+                        <h5>
+                          <b className="text-[#2F80ED]">Lisa baliom3 1100</b>
+                        </h5>
+                        <p className="mb-0 text-[#333] font-semibold">
+                          cameron philips :
+                        </p>
+                        <p className="mb-0 text-[#4F4F4F] text-sm">
+                          please check this out
+                        </p>
                       </div>
-                    ) : (
-                      <Avatar
-                        sx={{
-                          marginX: "0.7rem",
-                          bgcolor: "#2F80ED",
-                        }}
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="12"
-                          height="12"
-                          viewBox="0 0 12 12"
-                          fill="none"
-                        >
-                          <path
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
-                            d="M6 0C4.3425 0 3 1.3425 3 3C3 4.6575 4.3425 6 6 6C7.6575 6 9 4.6575 9 3C9 1.3425 7.6575 0 6 0ZM7.5 3C7.5 2.175 6.825 1.5 6 1.5C5.175 1.5 4.5 2.175 4.5 3C4.5 3.825 5.175 4.5 6 4.5C6.825 4.5 7.5 3.825 7.5 3ZM10.5 10.5C10.35 9.9675 8.025 9 6 9C3.9825 9 1.6725 9.96 1.5 10.5H10.5ZM0 10.5C0 8.505 3.9975 7.5 6 7.5C8.0025 7.5 12 8.505 12 10.5V12H0V10.5Z"
-                            fill="white"
-                            fill-opacity="0.54"
-                          />
-                        </svg>
-                      </Avatar>
-                    )}
-                    <div className="flex flex-col">
-                      <h5>
-                        <b className="text-[#2F80ED]">Lisa baliom3 1100</b>
-                      </h5>
-                      <p className="mb-0 text-[#333] font-semibold">
-                        cameron philips :
-                      </p>
+                    </div>
+                    <div className="flex flex-col items-end justify-end align-top">
                       <p className="mb-0 text-[#4F4F4F] text-sm">
-                        please check this out
+                        21/09/2021 10:45
                       </p>
                     </div>
                   </div>
